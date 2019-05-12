1318309721714b714e3e1bde211b3c14660fb6bd
AlbericoD <a.diasbarreto@icloud.com>
2019-05-11 22:31:06 -0300

    Updates

---
+++ precache-manifest.98665651bfd36dc2d4213c67603f64c0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fe0b1c61eaf8ff04f35aa5c679cc77",
+    "url": "/dashv2/index.html"
+  },
+  {
+    "revision": "e89e46dab7cd30cbc645",
+    "url": "/dashv2/static/css/1.c2c07fc8.chunk.css"
+  },
+  {
+    "revision": "05c4a961fdd81be3b847",
+    "url": "/dashv2/static/css/4.4333da42.chunk.css"
+  },
+  {
+    "revision": "b649e9c37333d0232cbd",
+    "url": "/dashv2/static/css/5.c0e5c83f.chunk.css"
+  },
+  {
+    "revision": "7d1f7c05821aea5f88cc",
+    "url": "/dashv2/static/css/6.5d5e9066.chunk.css"
+  },
+  {
+    "revision": "cf7a8ac16256cb8f916d",
+    "url": "/dashv2/static/css/7.9992ac66.chunk.css"
+  },
+  {
+    "revision": "0c6f51f8b9103e50c8c2",
+    "url": "/dashv2/static/css/main.e5c9e9a6.chunk.css"
+  },
+  {
+    "revision": "934010e2274a052e1e7e",
+    "url": "/dashv2/static/js/0.9c90d624.chunk.js"
+  },
+  {
+    "revision": "e89e46dab7cd30cbc645",
+    "url": "/dashv2/static/js/1.772b001a.chunk.js"
+  },
+  {
+    "revision": "05c4a961fdd81be3b847",
+    "url": "/dashv2/static/js/4.432bfdab.chunk.js"
+  },
+  {
+    "revision": "b649e9c37333d0232cbd",
+    "url": "/dashv2/static/js/5.ee0e037c.chunk.js"
+  },
+  {
+    "revision": "7d1f7c05821aea5f88cc",
+    "url": "/dashv2/static/js/6.fd89c3f5.chunk.js"
+  },
+  {
+    "revision": "cf7a8ac16256cb8f916d",
+    "url": "/dashv2/static/js/7.2ea5a1dd.chunk.js"
+  },
+  {
+    "revision": "89b80ff81fe8f098a462",
+    "url": "/dashv2/static/js/8.ae0301bb.chunk.js"
+  },
+  {
+    "revision": "0c6f51f8b9103e50c8c2",
+    "url": "/dashv2/static/js/main.1389c582.chunk.js"
+  },
+  {
+    "revision": "e7f0e89e5aa4c810fd62",
+    "url": "/dashv2/static/js/runtime~main.e7b51b3a.js"
+  }
+]);
